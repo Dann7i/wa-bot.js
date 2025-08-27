c1fb212b34dc4860c5b52779a78cbd65410fa963
Dann7i <hermawanwildan78@gmail.com>
2025-08-27 22:16:40 +0700

    update

--- logs.js
+++
@@ -1,9 +0,0 @@
-export async function eksekusi(pesan, gokubot) {
-  console.log(`pesan di terima: ${pesan}`);
-  if (gokubot) {
-    console.log(`pesan dikirim oleh id: ${gokubot.id}`);
-    const waktuSekarang = new Date();
-    console.log(`waktu pemrosesan ${waktuSekrang.toLocaleString()}`);
-    //logic bot
-  }
-}
